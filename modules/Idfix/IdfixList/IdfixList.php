b71f7729e36bac693d8a4cba0eb96a401d19a8ad
Wim J. Tol <ducopro2@gmail.com>
2014-02-23 16:06:33 +0100

    Idfix
    Worked on the display list

--- modules/Idfix/IdfixList/IdfixList.php
+++ modules/Idfix/IdfixList/IdfixList.php
@@ -74,12 +74,14 @@ class IdfixList extends Events3Module
         // Access to each field is checked so we can be sure these
         // fields can be showed
         $aColumns = $this->GetColumns($aTableConfig);
+        // Now postprocess the columns to give us a nice tableheader
+        $aHeader = $this->GetColumnsHeader($aColumns);
         // Now give us the raw dataset with the values
         // from the Idfix table
         $aDataSet = $this->GetRawDataset();
         // Than postprocess this dataset to provide the
         // exact HTML for display purposes
-        $aDisplayDataset = $this->GetDisplayDataset($aDataSet);
+        $aDisplayDataset = $this->GetDisplayDataset($aDataSet, $aColumns, $aTableConfig );
         // Now build the template variables
         $aData['aHead'] = $aColumns;
         $aData['aBody'] = $aDisplayDataset;
@@ -186,14 +188,114 @@ class IdfixList extends Events3Module
         }
         return $aColumns;
     }
-
+    
+    /**
+     * Postp[roces the array of headers.
+     * Trigger an event so we cab change the headers
+     * For example if we need sorting. We can make the headers
+     * clickable and add an icon for showing the sort order.
+     * 
+     * @param array $aColumns
+     * @return array $aHeaders HTML to render in the column header.
+     */
+    private function GetColumnsHeader($aColumns)
+    {
+      $aHeader = $aColumns;
+      $this->oIdfix->Event('ListHeader', $aHeader);
+      return $aHeader;
+    }
+    
+    /**
+     * Return the full datset from the idfix table.
+     * - Construct the right parameters for the
+     * IdfixStorage->LoadAllRecords() API function
+     * 
+     * @return
+     */
     private function GetRawDataset()
     {
+        $aConfig = $this->oIdfix->aConfig;
+        $cTable = $this->oIdfix->cTableName;
+        $aTableConfig = $aConfig['tables'][$cTable];
+        /* Build TypeID */
+        $iTypeID = (integer) $aTableConfig['id'];
+        /* Build ParentID */
+        $iParentID = $this->oIdfix->iParent;
+        /* Build a default Order
+           @TODO read the default sort order from the config
+         */
+        $cOrder = 'Weight';
+        /* Build the default where clauses*/
+        $aWhere = array();
+        /* Build the limit clause as an array for convenience
+           of the event handler, remember limit is 0-based*/
+        $iPage = $this->oIdfix->iObject;
+        $iRecsPerPage = $aTableConfig['pager']; 
+        $iStart = ( ($iPage-1)* $iRecsPerPage);
+        $aLimit = array( $iStart, $iRecsPerPage);   
+        
+        // Create a PACKAGE for the eventhandler
+        $aPack = array(
+          'type'=> $iTypeID ,
+          'parent'=> $iParentID ,
+          'order'=> $cOrder ,
+          'where'=> $aWhere ,
+          'limit'=> $aLimit ,
+        );
+        $this->oIdfix->Event( 'ListDataSet', $aPack);
+        
+        /* Now get all the values back in variables */
+        $iTypeID = $aPack['type'];
+        $iParentID = $aPack['parent'];
+        $cOrder = $aPack['order'];
+        $aWhere = $aPack['where'];
+        $cLimit = implode(',',  $aPack['limit']);
+        
+        $aRawDataSet = $this->oIdfixStorage->LoadAllRecords( $iTypeID, $iParentID, $cOrder, $aWhere, $cLimit );        
         return array();
     }
-    private function GetDisplayDataset()
+    
+    /**
+     * Postprocess every row in the raw dataset and extract
+     * only the fields we need.
+     * Than use the field-classes to get a visual representation
+     * 
+     * @param mixed $aRawSet
+     * @param mixed $aColumns
+     * @return
+     */
+    private function GetDisplayDataSet( $aRawSet, $aColumns, $aTableConfig )
     {
-        return array();
+        $aDisplaySet = array();
+        foreach($aRawSet as $aRawRow ) {
+            $aDisplayRow = array();
+            foreach( $aColumns as $cFieldName => $cColumnName ) {
+                // What is the value we need displayed
+                $xFieldData = $aRawRow[ $cFieldName ];
+                // And the matching field configuration?
+                $aFieldConfig = $aTableConfig['fields'][$cFieldName];
+                // Now it's the time to get the visual
+                $aDisplayRow[] = $this->GetDisplayDataCell( $xFieldData, $aFieldConfig);
+            }
+            $aDisplaySet[] = $aDisplayRow;
+        }
+        return $aDisplaySet;
+    }
+    
+    /**
+     * Given a fieldconfiguration and a value
+     * return the display
+     * 
+     * @todo Create eventhandler to return the right value
+     * 
+     * @param mixed $xFieldData
+     * @param mixed $aFieldConfig
+     * @return void
+     */
+    private function GetDisplayDataCell( $xFieldData, $aFieldConfig) {
+        $aFieldConfig['__RawValue'] = $xFieldData;
+        $this->oIdfix->Event('DisplayField', $aFieldConfig);
+        return $aFieldConfig['__RawValue'];
     }
 
 }
